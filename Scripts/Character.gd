0059b1bfbe24c8e15c64b0254adba29245775833
Matheus <matheus.cumpian@hotmail.com>
2019-02-09 13:13:39 -0200

    Implemented Security Cameras

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
 const SPEED = 10
-const MAX_SPEED = 200
+var MAX_SPEED = 200.0
 const FRICTION = 0.1
 
 
 func _ready():
 
 	pass
-
+	
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,49 @@
 extends "res://Scripts/Character.gd"
 
-var move = Vector2()
+var motion = Vector2()
 
 func _ready():
-
-
-
-
 	pass
 
-func _process(delta):	
+
+func _process(delta):
 	
-	if Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down") :
-		move += Vector2(0,-1)
-	if Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
-		move += Vector2(0,1)
+#	ligar_lanterna()
+	
+	pass
+
+func _physics_process(delta):
+	
+	update_motion(delta)
+	move_and_slide(motion)
+	
+	pass
+
+func update_motion(delta):
+	
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
+		motion.y = clamp((motion.y - SPEED), -MAX_SPEED , 0)
+	elif Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
+		motion.y = clamp((motion.y + SPEED), 0 , MAX_SPEED)
+	else:
+		motion.y = lerp(motion.y, 0 , FRICTION)
 	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
-		move += Vector2(-1,0)
-	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
-		move += Vector2(1,0)
-		
-	move_and_slide(move)
-	
-	
-	
+		motion.x = clamp((motion.x - SPEED), -MAX_SPEED , 0)
+	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = clamp((motion.x + SPEED), 0 , MAX_SPEED)
+	else:
+		motion.x = lerp(motion.x, 0 , FRICTION)
 	pass
+	
+#func ligar_lanterna():
+#
+#	if Input.is_action_just_pressed("toggle_flashlight"):
+#		if !$Lanterna.enabled == false:
+#
+#			$Lanterna.enabled = false
+#		else:
+#			$Lanterna.enabled = true
+#	pass
+
